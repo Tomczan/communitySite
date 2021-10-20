9b1b8543e37dded9fdeedd4c6085bbb74e9288f2
Tomasz Czuba <t.czuba95@gmail.com>
2021-10-20 18:01:44 +0200

    Login form typo corrected

--- account/views.py
+++ account/views.py
@@ -6,17 +6,20 @@ from .forms import LoginForm
 
 # Create your views here.
 
+
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
 
-            if user in not None:
+            if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Uwierzytelnianie zakończyło się sukcesem.')
+                    return HttpResponse(
+                        'Uwierzytelnianie zakończyło się sukcesem.')
                 else:
                     return HttpResponse('Konto jest zablokowane.')
             else:

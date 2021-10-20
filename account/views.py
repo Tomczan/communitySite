8b31b1e1ad21ba64b00ed0a22c5c389b6799bdf0
Tomasz Czuba <t.czuba95@gmail.com>
2021-10-20 17:31:44 +0200

    Basic login view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+
+            if user in not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Uwierzytelnianie zakończyło się sukcesem.')
+                else:
+                    return HttpResponse('Konto jest zablokowane.')
+            else:
+                return HttpResponse('Nieprawidlowe dane.')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})

62276367e821f1a5717657b298d34f4df9a75907
Tomasz Czuba <t.czuba95@gmail.com>
2021-10-27 13:32:48 +0200

    Register account form, urls, views and templates created

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -33,3 +33,20 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # utworzenie nowego uzytkownika, jednak jeszcze nie zapisanego do bd
+            new_user = user_form.save(commit=False)
+            # ustawienie hasła
+            new_user.set_password(user_form.cleaned_data['password'])
+            # zapisanie uzytkownika
+            new_user.save()
+            return render(request, 'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})

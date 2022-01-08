c64ec6c7ee313cb700d2a1518e8f8bae6e0f41ec
Tomasz Czuba <t.czuba95@gmail.com>
2022-01-08 15:28:35 +0100

    add: urls in account app to handle user list and detail
    add: ABSOLUTE_URL_OVERRIDES in project settings

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,3 +147,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
 ]
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda user: reverse_lazy('user_detail', args=[user.username])
+}

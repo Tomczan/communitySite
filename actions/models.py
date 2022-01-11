20bab654278e0552bf00e5153359ece7378be8d2
Tomasz Czuba <t.czuba95@gmail.com>
2022-01-11 17:01:57 +0100

    add: ContentType fields to Action model to handle generic relations

--- actions/models.py
+++ actions/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
@@ -11,6 +13,16 @@ class Action(models.Model):
                              on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(null=True,
+                                            blank=True,
+                                            db_index=True)
+    target = GenericForeignKey('target_ct', 'target_id')
 
     class Meta:
         ordering = ('-created', )

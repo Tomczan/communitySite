91295e71368f8281b3840b39ae4c3cefcbb9b055
Tomasz Czuba <t.czuba95@gmail.com>
2022-01-16 15:06:52 +0100

    add: redis database settings
    update: requirements.txt

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -90,6 +90,12 @@ DATABASES = {
     }
 }
 
+# Redis database
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 

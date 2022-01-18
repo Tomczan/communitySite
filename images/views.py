728af6c558154de68971b7964274bb7cc5ca10b9
Tomasz Czuba <t.czuba95@gmail.com>
2022-01-18 19:02:00 +0100

    add: images ranking based on views

--- images/views.py
+++ images/views.py
@@ -47,6 +47,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # incr inkrementuje wartosc klucza o 1, jezeli nie istnieje to go tworzy
     total_views = r.incr('image:{}:views'.format(image.id))
+    # inkrementacja o 1 rankingu danego obrazu
+    r.zincrby('image_ranking', 1, image.id)
     return render(request, 'images/image/detail.html', {
         'section': 'images',
         'image': image,
@@ -95,4 +97,18 @@ def image_list(request):
     return render(request, 'images/image/list.html', {
         'section': 'images',
         'images': images
+    })
+
+
+@login_required
+def image_ranking(request):
+    # pobranie slownika rankingu obrazow
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # pobranie najczesciej wyswietlanych obrazow
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {
+        'section': 'images',
+        'most_viewed': most_viewed
     })
